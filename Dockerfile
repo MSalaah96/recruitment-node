43b081ed32f61b3e39fe4a78a75fa8d0ae043750
MSalaah96 <mohammedsalaah96@gmail.com>
2022-11-12 14:30:00 +0200

    infrastructure

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+FROM node:18-alpine AS development
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install glob rimraf
+
+RUN npm install
+
+COPY . .
+
+RUN npm run build
+
+FROM node:18-alpine as production
+
+ARG NODE_ENV=production
+ENV NODE_ENV=${NODE_ENV}
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install --only=production
+
+COPY . .
+
+COPY --from=development /app/dist ./dist
+
+EXPOSE 3000
+
+CMD ["node", "dist/main"]
---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+mkdir -p ./gateway/certs
+cd ./gateway/certs
+mkcert carbon.local.com "*.carbon.local.com"
+
+#TODO: Load and update hosts dynamically from docker-compose file
+declare -a hosts=("182.39.0.2 carbon.database.local.com" "182.39.0.1 carbon.local.com" "182.39.0.3 carbon.api.local.com")
+
+# get number of hosts
+numberOfHosts=${#hosts[@]}
+
+# use for loop to read all values and indexes
+for (( i = 0; i < ${numberOfHosts}; i++ ));
+do
+  if ! grep -q "${hosts[$i]}" /etc/hosts; then
+    echo "${hosts[$i]}" | sudo tee -a /etc/hosts > /dev/null
+  fi
+done
+
+docker-compose up -d --build
