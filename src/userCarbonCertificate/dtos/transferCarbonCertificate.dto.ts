90af8d28e33a3ed326737b938622846b1b9a3a5f
MSalaah96 <mohammedsalaah96@gmail.com>
2022-11-12 20:38:07 +0200

    hot fix for ApiProperty

--- src/userCarbonCertificate/dtos/transferCarbonCertificate.dto.ts
+++ src/userCarbonCertificate/dtos/transferCarbonCertificate.dto.ts
@@ -1,8 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 export class TransferCarbonCertificateDto {
+  @ApiProperty({
+    description: `user id the certificate will be transferred to`,
+    example: `c3b296f8-fa1f-419c-8c2b-1a5cb5c53f4b`,
+  })
   @IsString()
   @IsNotEmpty()
   userId: string;
+  @ApiProperty({
+    description: `certificate id that will be transferred`,
+    example: `c3b296f8-fa1f-419c-8c2b-1a5cb5c53f4b`,
+  })
   @IsString()
   @IsNotEmpty()
   carbonCertificateId: string;
